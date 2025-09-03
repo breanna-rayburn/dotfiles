a6a0c5678ab7f022c3bc5d235d673e6bb1b3b124
Breanna Rayburn <breanna.rayburn@proton.me>
2025-09-03 16:31:18 -0700

    add useful autocmds

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,7 +1,39 @@
+-- close nvim-tree if it's last buffer open
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	callback = function()
+		if #vim.api.nvim_list_bufs() == 1 and vim.bo.filetype == "NvimTree" then
+	vim.cmd("quit")
+	end
+	end,
+})
+
 -- set startuptime
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     local startuptime = vim.fn.reltimefloat(vim.fn.reltime(vim.g.start_time))
     vim.g.startup_time_ms = string.format("%.2f ms", startuptime * 1000)
   end,
+})
+
+-- disable automatic comment on newline
+vim.api.nvim_create_autocmd("FileType", {
+		pattern = "*",
+		callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+		end,
+})
+
+-- run alpha on empty
+vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BDeletePre *",
+	group = "alpha_on_empty",
+	callback = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local name = vim.api.nvim_buf_get_name(bufnr)
+		if name == "" then
+      vim.cmd([[:Alpha | bd#]])
+		end
+	end,
 })
