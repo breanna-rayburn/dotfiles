fe67614d8ed0fe92902c5455c0b08601482ff043
Breanna Rayburn <breanna.rayburn@proton.me>
2025-09-03 23:27:20 -0700

    add jsonc to treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
+	ensure_installed = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "jsonc", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
