66e304bd33b9a6f90538d89c3cbecfdd981d7fc6
Breanna Rayburn <breanna.rayburn@proton.me>
2025-09-09 12:49:28 -0700

    add options

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -2,8 +2,15 @@
 setopt append_history inc_append_history share_history # better history
 setopt auto_menu menu_complete # autocmp first menu match
 setopt autocd # type a dir to cd
+setopt auto_param_slash # when a dir is completed, add a / instead of a trailing space
 setopt no_case_glob no_case_match # make cmp case insensitive
+setopt extended_glob # match ~ # ^
 
 # completion options
 zstyle ':completion:*' menu select # tab opens cmp menu
 zstyle ':completion:*' special-dirs true # force . and .. to show in cmp menu
+zstyle ':completion:*' squeeze-slashes false # explicit disable to allow /*/ expansion
+
+# History
+HISTFILE="$XDG_CACHE_HOME/zsh_history" # move histfile to cache
+HISTCONTROL=ignoreboth # consecutive duplicates & commands starting with space are not saved
--- .zshrc
+++ .zshrc
@@ -7,9 +7,5 @@
 [[ -f ~/.zsh/nvm.zsh ]] && source ~/.zsh/nvm.zsh
 [[ -f ~/.zsh/options.zsh ]] && source ~/.zsh/options.zsh
 
-# History
-HISTFILE="$XDG_CACHE_HOME/zsh_history" # move histfile to cache
-HISTCONTROL=ignoreboth # consecutive duplicates & commands starting with space are not saved
-
 # Load Starship
 eval "$(starship init zsh)"
